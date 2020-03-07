7ce73f955b05983e97bada8d9337e73c06783701
netherwave <netherwave63@gmail.com>
2020-03-07 10:01:02 +0800

    Update CORS allow methods

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT', 'POST', 'DELETE')
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE')
   }
   next()
 })
