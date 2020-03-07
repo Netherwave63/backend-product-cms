14b08938ceaa7eb6af928f831f6788817f2038b8
netherwave <netherwave63@gmail.com>
2020-03-07 09:13:58 +0800

    Allow CORS from http

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const app = express()
 
 connectDB()
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://products-cms.netlify.com')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, 'public')))
